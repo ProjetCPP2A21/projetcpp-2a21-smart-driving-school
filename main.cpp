923f997d442515819084597013b0f0c34d0cfcd7
adambhadj <68235790+adambhadj@users.noreply.github.com>
2025-09-29 18:24:32 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
-#include "moniteur.h"
+#include "mainwindow.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    moniteur w;
+    MainWindow w;
     w.show();
     return a.exec();
 }
