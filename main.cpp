f946f5503c6e5e7f08b9149761b896b34d66e8ee
islemmelki <melkiislem71@gmail.com>
2025-09-30 12:20:38 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
-#include "examens.h"
+#include "apprenant.h"
 
 #include <QApplication>
-#include <QLocale>
-#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QTranslator translator;
-    const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
-        const QString baseName = "examens_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
-            a.installTranslator(&translator);
-            break;
-        }
-    }
-    examens w;
+    apprenant w;
     w.show();
     return a.exec();
 }
